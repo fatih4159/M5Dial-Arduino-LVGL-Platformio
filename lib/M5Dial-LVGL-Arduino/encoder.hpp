47d5a2d3fcf9ee0cf6b4edb94b67b0967eb859c8
fatih4159 <78727371+fatih4159@users.noreply.github.com>
2026-09-04 22:08:05 +0200

    fix: align encoder pins with M5 Dial hardware definition

--- lib/M5Dial-LVGL-Arduino/encoder.hpp
+++ lib/M5Dial-LVGL-Arduino/encoder.hpp
@@ -1,5 +1,4 @@
-// SPDX-License-Identifier: MIT
-// SPDX-FileCopyrightText: Copyright 2024 mzyy94
+#pragma once
 
 #include <driver/pcnt.h>
 #include <driver/gpio.h>
@@ -9,13 +8,13 @@ class Encoder
 private:
     gpio_num_t _pin_a;
     gpio_num_t _pin_b;
-
     pcnt_unit_t _pcnt_unit;
 
 public:
     Encoder();
     ~Encoder();
-    void setup(gpio_num_t pin_a = GPIO_NUM_40, gpio_num_t pin_b = GPIO_NUM_41);
+    // M5Stack M5 Dial: encoder A = GPIO41, encoder B = GPIO40.
+    void setup(gpio_num_t pin_a = GPIO_NUM_41, gpio_num_t pin_b = GPIO_NUM_40);
     int getCount(bool clear = false);
     void reset();
 };
